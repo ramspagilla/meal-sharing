804ddd6f060196dbd8a221a8bf4cc354eba5fa62
rap <rap@netcompany.com>
2022-11-09 22:20:32 +0100

    initial commit

---
+++ src/backend/server.js
@@ -0,0 +1,8 @@
+const app = require("./app");
+
+// @ts-ignore
+const port = parseInt(process.env.PORT, 10) || process.env.API_PORT;
+
+app.listen(port, () => {
+  console.log(`Backend api available at ${process.env.API_PATH}`);
+});
